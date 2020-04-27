30a4c2d655f634f84da9cfdc4f912ca80757afa9
Eugene Furmanov <codeinsack@gmail.com>
2020-04-27 21:40:04 +0300

    add AppProps and AppState

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,43 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 
-class App extends Component {
+interface AppProps {
+  color?: string
+}
+
+interface AppState {
+  counter: number
+}
+
+class App extends Component<AppProps, AppState> {
+  constructor(props: AppProps) {
+    super(props);
+
+    this.state = {
+      counter: 0,
+    }
+  }
+
+  onIncrement = (): void => {
+    this.setState({
+      counter: this.state.counter + 1,
+    })
+  }
+
+  onDecrement = (): void => {
+    this.setState({
+      counter: this.state.counter - 1,
+    })
+  }
+
   render() {
-    return <div>Hi there!</div>
+    return (
+      <div>
+        <button onClick={this.onIncrement}>Increment</button>
+        <button onClick={this.onDecrement}>Decrement</button>
+        {this.state.counter}
+      </div>
+    )
   }
 }
 
